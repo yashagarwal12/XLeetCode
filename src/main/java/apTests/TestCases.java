0ef4d66645db30cb194f060779db9694541a7d12
atribiswas <atribiswas@Atris-MacBook-Air.local>
2023-10-21 19:09:13 +0530

    updated changes - 2023-10-21 19:09:13

---
+++ src/main/java/apTests/TestCases.java
@@ -0,0 +1,57 @@
+
+package apTests;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+//Selenium Imports
+import java.util.logging.Level;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+///
+
+public class TestCases {
+    WebDriver driver;
+
+    public TestCases() {
+        System.out.println("Constructor: TestCases");
+
+        WebDriverManager.chromedriver().timeout(30).setup();
+        ChromeOptions options = new ChromeOptions();
+        LoggingPreferences logs = new LoggingPreferences();
+
+        // Set log level and type
+        logs.enable(LogType.BROWSER, Level.ALL);
+        logs.enable(LogType.DRIVER, Level.ALL);
+        options.setCapability("goog:loggingPrefs", logs);
+
+        // Set path for log file
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "chromedriver.log");
+
+        driver = new ChromeDriver(options);
+
+        // Set browser to maximize and wait
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    public void endTest() {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    public void testCase01() {
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://www.google.com");
+        System.out.println("end Test case: testCase01");
+    }
+
+}
+
